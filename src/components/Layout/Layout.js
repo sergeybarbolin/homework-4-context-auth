b898d4090792994445fb6633d2f62c070a88482e
sergeybarbolin <sergeybarbolin25@gmail.com>
2019-08-10 23:56:59 +0300

    react context

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,43 @@
-import React, { PureComponent } from 'react';
+import React, { Fragment, PureComponent } from 'react';
+import SectionTitle from './../SectionTitle';
 import './Layout.css';
 
 class Layout extends PureComponent {
+  renderHeader = Children => (
+    <header className="header">
+      <SectionTitle className="header__title">Header</SectionTitle>
+      {Children && (<div className="header__content"><Children/></div>)}
+    </header>
+  )
+
+  renderFooter = Children => (
+    <footer className="footer">
+      <p className="header__title section-title">Footer</p>
+      <Children/>
+    </footer>
+  )
+
   render() {
-    return 'empty';
+    const {header, footer, children} = this.props
+
+    return (
+      <Fragment>
+        { header && this.renderHeader(header) }
+
+        <main 
+          className={`
+            main 
+            ${header ? 'main--with-header' : ''} 
+            ${footer ? 'main--with-footer' : ''} 
+          `}
+        >
+          <SectionTitle className="main__title">Main</SectionTitle>
+          {children}
+        </main>
+
+        { footer && this.renderFooter(footer) }
+      </Fragment>
+    )
   }
 }
 
